19124664cf8d3e082af787c0e6ef92cd35c08696
zinmyoswe <zinmyoswe@github.com>
2024-04-11 07:24:00 +0630

    finish Node and React

---
+++ react.sh
@@ -0,0 +1,38 @@
+#installtion
+npx create-react-app reactlearn
+cd my-app
+npm start
+
+
+#like index.php(main file)
+src>app.js
+
+-------------------------------------------------------------------------------------------------
+#typescript installtion
+
+#It prompts you to select a project template (such as Vue, React, or Vanilla JavaScript) and guides you through setting up your project.
+npm create vite@latest
+#can choose typescript or javascript
+install tailwindcss with vite and do per instruction
+
+install react router doom
+
+#Tailwind Component
+https://tailwindui.com/components/application-ui/navigation/navbars
+
+#install axios
+npm i axios
+
+UseEffect is app first load
+
+
+----------------------------------------------------------------------------------------------------
+npm create vite .
+
+npm i
+
+#running server
+npm run dev
+
+#bootstrap installation
+npm i react-router-dom bootstrap react-bootstrap
